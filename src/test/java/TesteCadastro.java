0838944922c83da45a427267078219a626f58265
eltonccdantas <elton.chagas.dantas@gmail.com>
2024-04-13 20:58:36 -0300

    Refactor TesteCadastro.java

--- src/test/java/TesteCadastro.java
+++ src/test/java/TesteCadastro.java
@@ -1,4 +1,6 @@
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
@@ -8,74 +10,61 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class TesteCadastro {
+
+    WebDriver driver = new FirefoxDriver();
+
+    @Before
+    public void inicializa() {
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+    }
+
+    @After
+    public void finaliza() {
+        driver.quit();
+    }
+
     @Test
     public void deveValidarNomeObrigatorio() {
-        WebDriver driver = new FirefoxDriver();
-        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-
         driver.findElement(By.id("elementosForm:cadastrar")).click();
         Alert alert = driver.switchTo().alert();
         String texto = alert.getText();
         Assert.assertEquals("Nome eh obrigatorio", texto);
-        
-        driver.quit();  
     }
 
     @Test
     public void deveValidarSobrenomeObrigatorio() {
-        WebDriver driver = new FirefoxDriver();
-        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-
         driver.findElement(By.id("elementosForm:nome")).sendKeys("Elton");
-        
         driver.findElement(By.id("elementosForm:cadastrar")).click();
         Alert alert = driver.switchTo().alert();
         String texto = alert.getText();
         Assert.assertEquals("Sobrenome eh obrigatorio", texto);
-        
-        driver.quit();  
     }
 
     @Test
     public void deveValidarSexoObrigatorio() {
-        WebDriver driver = new FirefoxDriver();
-        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-        
         driver.findElement(By.id("elementosForm:nome")).sendKeys("Elton");
         driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Dantas");
-
         driver.findElement(By.id("elementosForm:cadastrar")).click();
         Alert alert = driver.switchTo().alert();
         String texto = alert.getText();
         Assert.assertEquals("Sexo eh obrigatorio", texto);
-        
-        driver.quit();  
     }
 
     @Test
     public void deveValidarComidaFavorita() {
-        WebDriver driver = new FirefoxDriver();
-        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-
         driver.findElement(By.id("elementosForm:nome")).sendKeys("Elton");
         driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Dantas");
         driver.findElement(By.id("elementosForm:sexo:0")).click();
         driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
         driver.findElement(By.id("elementosForm:comidaFavorita:3")).click();
-
         driver.findElement(By.id("elementosForm:cadastrar")).click();
         Alert alert = driver.switchTo().alert();
         String texto = alert.getText();
         Assert.assertEquals("Tem certeza que voce eh vegetariano?", texto);
-        
-        driver.quit();  
     }
 
     @Test
     public void deveValidarEsporte() {
-        WebDriver driver = new FirefoxDriver();
-        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-
         driver.findElement(By.id("elementosForm:nome")).sendKeys("Elton");
         driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Dantas");
         driver.findElement(By.id("elementosForm:sexo:0")).click();
@@ -89,7 +78,5 @@ public class TesteCadastro {
         Alert alert = driver.switchTo().alert();
         String texto = alert.getText();
         Assert.assertEquals("Voce faz esporte ou nao?", texto);
-        
-        driver.quit();  
     }
 }
